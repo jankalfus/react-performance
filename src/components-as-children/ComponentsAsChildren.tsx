9abd94282ebee2a1e11fe533d7a152f70bb9566f
Jan Kalfus <jank@kontent.ai>
2023-07-28 14:21:40 +0200

    Components as children

--- src/components-as-children/ComponentsAsChildren.tsx
+++ src/components-as-children/ComponentsAsChildren.tsx
@@ -1,3 +1,101 @@
+import { useState } from "react";
+import { SlowComponent } from "../shared/SlowComponent";
+
 export const ComponentsAsChildren = () => {
-  return null;
+  const [scrollY, setScrollY] = useState(0);
+  return (
+    <>
+      <div>scrollY (for debug): {scrollY}</div>
+      <div
+        onScroll={(e) => {
+          setScrollY(e.currentTarget.scrollTop);
+        }}
+        style={{
+          width: "95vw",
+          height: "50vh",
+          border: "1px solid black",
+          textAlign: "center",
+          overflowY: "scroll",
+        }}
+      >
+        <Menu collapsed={scrollY > 0} />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+        <SlowComponent />
+      </div>
+    </>
+  );
+};
+
+const Menu = ({ collapsed }: { collapsed: boolean }) => {
+  return (
+    <>
+      <div style={{ position: "absolute" }}>
+        {collapsed ? <CompactMenu /> : <FullMenu />}
+      </div>
+      <div style={{ marginTop: "calc(4rem + 42px)" }}></div>
+    </>
+  );
+};
+
+const MenuItems = ["Menu item 1", "Menu item 2", "Menu item 3"];
+const FullMenu = () => {
+  return (
+    <ul
+      style={{
+        listStyleType: "none",
+        display: "flex",
+        gap: "1rem",
+        padding: 0,
+        margin: "2rem",
+      }}
+    >
+      {MenuItems.map((item) => (
+        <li
+          key={item}
+          style={{
+            border: "1px solid black",
+            padding: "0.5rem",
+            background: "lightgrey",
+            cursor: "pointer",
+          }}
+        >
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const CompactMenu = () => {
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        margin: "2rem",
+        padding: "0.5rem",
+        background: "lightgrey",
+        cursor: "pointer",
+      }}
+    >
+      Hamburger
+    </div>
+  );
 };
--- src/shared/SlowComponent.tsx
+++ src/shared/SlowComponent.tsx
@@ -1,4 +1,9 @@
 export const SlowComponent = () => {
   for (let i = 0; i < 100000000; i++) {}
-  return null;
+
+  return (
+    <div style={{ border: "1px solid black", margin: "2rem", padding: "1rem" }}>
+      I’m a slow component :(
+    </div>
+  );
 };
