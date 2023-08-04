bfb8878049865bb86c6baa565e17a6a95ea8aa95
Jan Kalfus <jank@kontent.ai>
2023-08-04 15:08:16 +0200

    Improve components as props example

--- src/components-as-props/ComponentsAsProps.tsx
+++ src/components-as-props/ComponentsAsProps.tsx
@@ -5,15 +5,31 @@ export const ComponentsAsProps = () => {
   const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{
+        display: "flex",
+        overflowX: "hidden",
+        border: "1px solid grey",
+        paddingRight: 20,
+        position: "relative",
+      }}
+    >
       <div style={{ flex: 1 }}>
         <Content onShowDetailsClick={() => setShowDetails(true)} />
       </div>
-      {showDetails && (
-        <div style={{ flex: "0 0 400px", border: "1px solid grey" }}>
-          <Details onClose={() => setShowDetails(false)} />
-        </div>
-      )}
+      <div
+        style={{
+          flex: "0 0 400px",
+          border: "1px solid grey",
+          position: "absolute",
+          width: 300,
+          right: showDetails ? 0 : -280,
+          background: "white",
+          height: "calc(100% - 2 * 1px)",
+        }}
+      >
+        <Details onClose={() => setShowDetails(false)} />
+      </div>
     </div>
   );
 };
@@ -53,6 +69,9 @@ const Details = ({ onClose }: { onClose: () => void }) => {
   return (
     <div style={{ padding: "1rem" }}>
       <p>Details are here</p>
+      <SlowComponent />
+      <SlowComponent />
+      <SlowComponent />
       <button onClick={onClose}>Close</button>
     </div>
   );
