b3e149cbfb3f9f8cfcc74bb17c0821fcad0ddc6b
Jan Kalfus <jank@kontent.ai>
2023-08-04 14:18:34 +0200

    Add React.memo example

--- src/react-memo/ReactMemo.tsx
+++ src/react-memo/ReactMemo.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+
+const MaxItems = 50;
+const items = Array.from({ length: MaxItems }, (_, i) => ({
+  id: i,
+  name: `Item #${i + 1}`,
+}));
+
 export const ReactMemo = () => {
-  return null;
+  const [listItems, setListItems] = useState(items);
+
+  return (
+    <>
+      <input
+        onChange={() => {
+          setListItems((items) => {
+            const indexToRemove = Math.floor(Math.random() * items.length);
+            return items.filter((_, i) => i !== indexToRemove);
+          });
+        }}
+        placeholder="Hide random item by typing"
+      />
+      <ul>
+        {listItems.map((item) => (
+          <ListItem key={item.id} name={item.name} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const ListItem = ({ name }: { name: string }) => {
+  for (let i = 0; i < 10000000; i++) {} // do not touch
+
+  return <li>{name}</li>;
 };
